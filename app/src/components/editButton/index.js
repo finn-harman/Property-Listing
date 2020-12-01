0620293596e13a7b9b46e9e3f1244bfb93431e6a
FinnHarman <fh219@ic.ac.uk>
2020-12-01 15:13:35 +0000

    Simple toying around with Edit Price button

--- app/src/components/editButton/index.js
+++ app/src/components/editButton/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 class EditButton extends React.Component {
 
-  handleClick() {
-    fetch('/server/listings.json')
-      
+  async postData() {
+    const data = { postcode: "POSTCODE" };
+
+    // fetch('/server/listings.json/')
+    //   .then(res => res.json())
+    //   .then(res => console.log(res))
+
+    fetch('/server/listings.json', {
+      method: 'GET',
+    })
   }
 
   render() {
     return (
-      <button className="btn" onClick={this.handleClick}>
+      <button className="btn" onClick={this.postData}>
         Edit Price
       </button>
     )
